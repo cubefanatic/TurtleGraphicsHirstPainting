df973b3c1393dc089273f26d0370ce9c61014edd
cubefanatic <farhanthegamer162@gmail.com>
2022-05-18 12:57:47 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+import turtle
+from turtle import Turtle
+
+t = Turtle()
+t.hideturtle()
+t.speed("fastest")
+turtle.colormode(255)
+
+color_list = [(202, 164, 109), (238, 240, 245), (150, 75, 49), (223, 201, 135), (52, 93, 124), (172, 154, 40), (140, 30, 19),
+              (133, 163, 185), (198, 91, 71), (46, 122, 86), (72, 43, 35), (145, 178, 148), (13, 99, 71), (233, 175, 164),
+              (161, 142, 158), (105, 74, 77), (55, 46, 50), (183, 205, 171), (36, 60, 74), (18, 86, 90), (81, 148, 129),
+              (148, 17, 20), (14, 70, 64), (30, 68, 100), (107, 127, 153), (174, 94, 97), (176, 192, 209)]
+
+
+def paint():
+    for _ in range(10):
+        for _ in range(10):
+            color = random.choice(color_list)
+            t.dot(20, color)
+            t.penup()
+            t.forward(50)
+        t.setx(0)
+        current_y = t.ycor()
+        t.sety(current_y-50)
+        t.pendown()
+
+
+paint()
+screen = turtle.Screen()
+screen.exitonclick()
